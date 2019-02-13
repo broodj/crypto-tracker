c5c7457c0aefbe2900abf4cd6b8438104c7eb2e6
Brody <brodywilton@hotmail.com>
2019-02-13 23:06:34 +0000

    Add response to give current BTC price

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-
 app.listen(3000, function(){
   console.log('Server started on port 3000');
 });
@@ -18,13 +17,11 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-
-
 app.post('/', function(req, res){
-
-
-
   request('https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD', function(error, response, body){
-    console.log(body.open.hour);
+    var data = JSON.parse(body);
+    var price = data.last;
+
+    res.send(`The price of BTC is currently ${price}`);
   });
 });
